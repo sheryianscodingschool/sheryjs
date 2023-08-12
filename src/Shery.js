4203000ca9d856d0c9caac2581d7d461c8c40013
Aayush <aayushchouhan24@@gmail.com>
2023-08-12 21:50:22 +0530

    fixx

--- src/Shery.js
+++ src/Shery.js
@@ -36,7 +36,7 @@ function Shery() {
     if (!(elem.nodeName.toLowerCase() === 'img')) {
       fragment = fragment.replace('isMulti ;', `float c = sin((sin((uv.x+(time+snoise(vec3(uv,1.0)))*0.1 )* (20.0+uv.y) + snoise(vec3(uv,1.0)) ) / 15.0 + (snoise(vec3(uv,1.0))/10.)) + uv.y);gl_FragColor =mix(texture2D(uTexture[1], uv), texture2D(uTexture[0], uv), step((uScroll-.04 )-uSection, c + uv.y*(uv.y/100.0)));`)
       window.addEventListener('scroll', () => {
-        uniforms.uScroll.value = (window.scrollY / (window.innerHeight)) - .1
+        uniforms.uScroll.value = ((window.scrollY / (window.innerHeight)) - .1) - (window.scrollY / opts.target ? document.querySelector(opts.target).getBoundingClientRect().top : elem.getBoundingClientRect().top);
         const newSection = Math.floor((scrollY / window.innerHeight))
         if (newSection != uniforms.uSection.value) {
           if (t.length > newSection + 1) {
